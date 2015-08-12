08fac5943c1a15272777603d35e4cd8a9d214372
Joseph <jdecker@typhoondata.com>
2015-08-11 22:32:23 -0600

    trying to add about page...

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hello world!")
+    return HttpResponse("Rango says hello world! <a href='/rango/about'>About</a>")
+
+def about(request):
+    return HttpResponse("Rango Says: Here is the about page  <a href='/rango'>Rango</a>")
